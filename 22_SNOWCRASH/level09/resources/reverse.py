f42348731bb08bf671b3b072428552ca15125c5e
waseemnaseeven <waseem.naseeven@gmail.com>
2024-04-19 14:08:44 +0200

    level 08 and 09 done

---
+++ 22_SNOWCRASH/level09/resources/reverse.py
@@ -0,0 +1,21 @@
+import sys
+
+def reverse_string(input):
+    ret = ''
+    for i in range(len(input)):
+        reversed_char = chr(ord(input[i]) - i)
+        ret += reversed_char
+    return ret
+    # return input[::-1]
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 reverse.py <string to reverse>")
+        sys.exit(1)
+    
+    input_str = sys.argv[1]
+    reversed_string = reverse_string(input_str)
+    print("Reversed string:", reversed_string)
+
+if __name__ == "__main__":
+    main()
