8d6264f58595e9618c48a1402e1ee17a5f995026
waseemnaseeven <waseem.naseeven@gmail.com>
2024-06-06 12:15:50 +0200

    switch to VM ubuntu made with vagrant + p1 done for sure with eth1 interface created

--- 23_IOT/p1/scripts/worker.sh
+++ 23_IOT/p1/scripts/worker.sh
@@ -1,18 +1,27 @@
 
 #!/bin/bash
 
-GREEN="\033[32m"
+PURPLE="\033[35m"
 RED="\033[31m"
 RESET="\033[0m"
 
+apt-get update 
+apt-get install -y net-tools
+
+if sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.111/24 dev eth1 && sudo ip link set eth1 up; then
+        echo -e "${PURPLE}adding IP address on interface eth1 SUCCEEDED${RESET}"
+else
+        echo -e "${RED}adding IP address on interface eth1 FAILED${RESET}"
+fi
+
 if export INSTALL_K3S_EXEC="agent -s https://192.168.56.110:6443 -t $(cat /vagrant/token.env) --node-ip=192.168.56.111"; then
-        echo -e "${GREEN}export INSTALL_K3S_EXEC SUCCEEDED${RESET}"
+        echo -e "${PURPLE}export INSTALL_K3S_EXEC SUCCEEDED${RESET}"
 else
         echo -e "${RED}export INSTALL_K3S_EXEC FAILED${RESET}"
 fi 
 
 if curl -sfL https://get.k3s.io | sh -; then
-	echo -e "${GREEN}K3s WORKER installation SUCCEEDED${RESET}"
+	echo -e "${PURPLE}K3s WORKER installation SUCCEEDED${RESET}"
 else
 	echo -e "${RED}K3s WORKER installation FAILED${RESET}"
 fi
