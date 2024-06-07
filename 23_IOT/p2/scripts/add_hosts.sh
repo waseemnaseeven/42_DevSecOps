6dc96b3012daefba51af7bf28cb5fae5c347bc41
waseemnaseeven <waseem.naseeven@gmail.com>
2024-06-07 15:12:18 +0200

    p2 almost finish, ingress modified

--- 23_IOT/p2/scripts/add_hosts.sh
+++ 23_IOT/p2/scripts/add_hosts.sh
@@ -3,3 +3,5 @@
 echo -e "192.168.56.110 app1.com" | sudo tee -a /etc/hosts
 echo -e "192.168.56.110 app2.com" | sudo tee -a /etc/hosts
 echo -e "192.168.56.110 app3.com" | sudo tee -a /etc/hosts
+
+cat /etc/hosts
--- 23_IOT/p2/scripts/server.sh
+++ 23_IOT/p2/scripts/server.sh
@@ -6,7 +6,6 @@ RESET="\033[0m"
 
 apt-get update 
 apt-get install -y net-tools
-sudo alias k='kubectl'
 
 if sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up; then
         echo -e "${PURPLE}adding IP address on interface eth1 SUCCEEDED${RESET}"
@@ -24,10 +23,4 @@ if curl -sfL https://get.k3s.io | sh -; then
     echo -e "${PURPLE}K3s MASTER installation SUCCEEDED${RESET}"
 else
     echo -e "${RED}K3s MASTER installation FAILED${RESET}"
-fi
-
-if sudo cat /var/lib/rancher/k3s/server/token >> /vagrant/token.env; then
-    echo -e "${PURPLE}TOKEN SUCCESSFULLY SAVED${RESET}"
-else
-    echo -e "${RED}TOKEN SAVING FAILED${RESET}"
 fi
