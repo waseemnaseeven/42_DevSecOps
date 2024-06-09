1c7ff26681f16391f09ae3569306c6dfb90bc901
waseemnaseeven <waseem.naseeven@gmail.com>
2024-06-09 11:44:27 +0200

    scripts modified

--- 23_IOT/p2/scripts/server.sh
+++ 23_IOT/p2/scripts/server.sh
@@ -7,13 +7,8 @@ RESET="\033[0m"
 apt-get update 
 apt-get install -y net-tools
 
-if sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up; then
-        echo -e "${PURPLE}adding IP address on interface eth1 SUCCEEDED${RESET}"
-else
-        echo -e "${RED}adding IP address on interface eth1 FAILED${RESET}"
-fi
 
-if export INSTALL_K3S_EXEC="--write-kubeconfig-mode=0644 --tls-san wnaseeveS --node-ip 192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110 --cluster-init "; then
+if export INSTALL_K3S_EXEC="--write-kubeconfig-mode=0644 --tls-san wnaseeveS --node-ip 192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110 "; then
     echo -e "${PURPLE}export INSTALL_K3S_EXEC SUCCEEDED${RESET}"
 else
     echo -e "${RED}export INSTALL_K3S_EXEC FAILED${RESET}"
@@ -23,4 +18,10 @@ if curl -sfL https://get.k3s.io | sh -; then
     echo -e "${PURPLE}K3s MASTER installation SUCCEEDED${RESET}"
 else
     echo -e "${RED}K3s MASTER installation FAILED${RESET}"
+fi
+
+if sudo ip link add eth1 type dummy && sudo ip addr add 192.168.56.110/24 dev eth1 && sudo ip link set eth1 up; then
+        echo -e "${PURPLE}adding IP address on interface eth1 SUCCEEDED${RESET}"
+else
+        echo -e "${RED}adding IP address on interface eth1 FAILED${RESET}"
 fi
