3e54c9499f011172723c905c052af876bbb717a1
wnaseeve <wnaseeve@student.42.fr>
2024-06-12 16:48:25 +0200

    argo cd init done

--- 23_IOT/p3/scripts/init_argo.sh
+++ 23_IOT/p3/scripts/init_argo.sh
@@ -5,6 +5,11 @@ kubectl create namespace argocd && kubectl create namespace dev
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # kubectl config to use the newCluster
-kubectl config use-context k3d-newCluster
+# kubectl config use-context k3d-newCluster
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+# Username is "admin"
+
+#For the password
+kubectl get secret argocd-initial-admin-secret -n argocd -o yaml | grep "password" | awk '{ print $2 }' | base64 --decode
