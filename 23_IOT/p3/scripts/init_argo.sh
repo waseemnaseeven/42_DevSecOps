cc3c7d4e637bd0885983b45ce373c83e03f7215a
waseemnaseeven <waseem.naseeven@gmail.com>
2024-06-12 15:53:50 +0200

    scripts modified

--- 23_IOT/p3/scripts/init_argo.sh
+++ 23_IOT/p3/scripts/init_argo.sh
@@ -4,3 +4,7 @@ kubectl create namespace argocd && kubectl create namespace dev
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# kubectl config to use the newCluster
+kubectl config use-context k3d-newCluster
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
--- 23_IOT/p3/scripts/k3d_install.sh
+++ 23_IOT/p3/scripts/k3d_install.sh
@@ -6,14 +6,24 @@ sudo apt-get update -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 # Install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+# Cluster creation and context 
 k3d cluster create newCluster
 
 k3d kubeconfig get newCluster
 
-sudo chmod 777 /etc/rancher/k3s/k3s.yaml
+sudo chmod 666 /etc/rancher/k3s/k3s.yaml
 
 k3d kubeconfig get newCluster > /etc/rancher/k3s/k3s.yaml
 
