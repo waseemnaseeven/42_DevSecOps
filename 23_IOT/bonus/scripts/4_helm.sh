8632c1d4fcb10d80cc38384167daa29cc308d357
waseemnaseeven0 <waseem.naseeven@gmail.com>
2024-06-19 17:49:53 +0200

    connecting to gitlab done, have to deal with the traffic now

--- 23_IOT/bonus/scripts/4_helm.sh
+++ 23_IOT/bonus/scripts/4_helm.sh
@@ -5,3 +5,20 @@ chmod 755 get_helm.sh
 ./get_helm.sh
 rm get_helm.sh
 
+
+kubectl create namespace gitlab
+helm install gitlab gitlab/gitlab \
+  --set global.hosts.domain=gitlab.wnaseeve.fr \
+  --set certmanager-issuer.email=wnaseeve@student.42.fr \
+  --set global.hosts.https="false" \
+  --set global.ingress.configureCertmanager="false" \
+  --set gitlab-runner.install="false" \
+  --set nginx-ingress.controller.hostNetwork=true \
+  --set nginx-ingress.controller.kind=DaemonSet \
+  --namespace=gitlab
+
+kubectl get secret --namespace=gitlab gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
+
+kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s && echo ok
+
+# gitlabT glpat-yfRExLcy5G_yLAZbBwCW
