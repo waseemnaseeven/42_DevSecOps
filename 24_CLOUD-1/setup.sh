28801afec15d6d823effd4798db20ce3c89de16c
waseemnaseeven <waseem.naseeven@gmail.com>
2024-06-28 14:26:24 +0200

    ...

--- 24_CLOUD-1/setup.sh
+++ 24_CLOUD-1/setup.sh
@@ -7,7 +7,9 @@ RESET="\033[0m"
 
 echo -e "${GREEN} ~~  INSTALLING EVERY TOOLS ~~ ${RESET}"
 sudo apt-get update -y
-sudo apt-get install vim git curl ncdu ansible -y 
+sudo apt-get install vim git curl ncdu ansible python3-pip -y 
+
+
 
 echo -e "${GREEN} ~~  INSTALLING DOCKER ~~ ${RESET}"
 curl -fsSL https://get.docker.com -o get-docker.sh
