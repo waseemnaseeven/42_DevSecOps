1c849667634a78b6f645baee75d23d13cb2f18c6
waseemnaseeven <waseem.naseeven@gmail.com>
2025-05-28 14:37:34 +0200

    test commit

--- 29_BADASS/P1/image_builder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build . -t host_wnaseeve -f host_wnaseeve.dockerfile
-docker build router -t router_wnaseeve -f router/router_wnaseeve.dockerfile
--- 29_BADASS/P1/image_deleter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker image rm host 
-docker image rm router
-docker image ls
